fece64045424927338bf2c26de82ca8c9c53656a
azelout <azeldu94@gmail.com>
2025-05-17 17:04:16 +0200

    Correction du path

--- config/config.py
+++ config/config.py
@@ -1,6 +1,13 @@
 import yaml
+from os.path import abspath, dirname, join
 
-def load_config(config_path='K:/TIPE/fuoco/config/config.yaml'):
+def load_config(config_path="config.yaml"):
+    # Obtenir le répertoire du fichier config.py
+    config_dir = dirname(abspath(__file__))
+
+    # Construire le chemin absolu vers config.yaml
+    config_path = join(config_dir, config_path)
+
     with open(config_path, 'r') as file:
         return yaml.safe_load(file)
 
